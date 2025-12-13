7130145d7dc87293605701d7957e850fb46d7bd0
CMS <ch@rlie.xyz>
2025-12-13 22:20:56 +0000

    feat: add nessus

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf install -y hashcat
+# dnf install -y <package>
 
 # Use a COPR Example:
 #
@@ -18,6 +18,18 @@ dnf install -y hashcat
 # dnf5 -y install package
 # Disable COPRs so they don't end up enabled on the final image:
 # dnf5 -y copr disable ublue-os/staging
+
+### Install Nessus
+# Download Nessus RPM
+curl --request GET \
+  --url 'https://www.tenable.com/downloads/api/v2/pages/nessus/files/Nessus-10.11.0-el9.x86_64.rpm' \
+  --output '/tmp/Nessus-10.11.0-el9.x86_64.rpm'
+
+# Install Nessus
+dnf install -y /tmp/Nessus-10.11.0-el9.x86_64.rpm
+
+# Clean up RPM file
+rm -f /tmp/Nessus-10.11.0-el9.x86_64.rpm
 
 #### Example for enabling a System Unit File
 
